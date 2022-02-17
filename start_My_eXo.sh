8ca2069486eb379265b38f112ae4dcebe6a4d493
bourasom <obouras@exoplatform.com>
2022-02-17 12:50:26 +0100

    fix bytecode issue due to changed jdk version

--- start_My_eXo.sh
+++ start_My_eXo.sh
@@ -8,9 +8,9 @@ if [ "$APP_MODE" = "preprod" ]; then
   #apply evolutions on database schema
   play evolutions:apply --%preprod
   #run app
-  play start --%preprod -Xmx1g -Xms1g
+  play start --%preprod -Xmx1g -Xms1g -noverify
 else  
-  play start --%prod -Xmx1g -Xms1g
+  play start --%prod -Xmx1g -Xms1g -noverify
 fi
 
 tail -f /dev/null
